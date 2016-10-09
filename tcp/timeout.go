c2bd31a68fc9b10094b2ee8353c3a793ee8a9af5
Joshua Liebow-Feeser <hello@joshlf.com>
2016-10-09 14:02:41 -0700

    tcp: Fixed timeout bug; added timeout documentation

--- tcp/timeout.go
+++ tcp/timeout.go
@@ -7,6 +7,37 @@ import (
 	"time"
 )
 
+// Timeouts are handled using a timeouter, which runs a single daemon
+// goroutine that checks to see when the next timeout will occur,
+// sleeps until that time, and then performs whatever action is
+// associated with that timeout.
+//
+// Timeouts may also be cancelled. In these cases, it's desirable to
+// avoid having the daemon acquire the global lock on the Conn
+// when it's technically not necessary (having found the timeout
+// cancelled, the daemon will then just release the lock without
+// doing any work). Thus, each timeout object has a cancel field.
+// When a timeout is cancelled, the goroutine doing the cancelling
+// atomically sets the cancel field to 1. Then, when the daemon
+// wakes up from sleep, it atomically loads the cancel field. If the
+// field has been set to 1, then the daemon simply throws away the
+// timeout object, and waits for the next timeout. If the field is
+// still 0, the daemon acquires the global Conn lock. However, there's
+// a chance that in between the atomic load of the cancel field
+// and acquiring the Conn lock, another goroutine acquired the
+// Conn lock, did some work, and canceled the timeout. Thus, after
+// acquiring the Conn lock, the daemon must re-check the cancel field.
+// If the cancel field is 1, the daemon immediately releases the
+// global Conn lock, throws the timeout away, and waits for the next
+// timeout.
+//
+// It is the responsibility of the goroutine doing the cancelling
+// to clear any record of the timeout from the Conn object. If a
+// timeout has not been cancelled by the time that the daemon
+// acquires the global Conn lock, then the timeout's callback is
+// executed. In this case, it is the responsibility of the callback
+// to clear any record of the timeout from the Conn object.
+
 type timeout struct {
 	f      func()
 	t      time.Time
@@ -59,7 +90,11 @@ func (t *timeouter) daemon() {
 		if atomic.LoadUint32(&to.cancel) == 0 {
 			// it wasn't cancelled
 			t.conn.mu.Lock()
-			to.f()
+			if atomic.LoadUint32(&to.cancel) == 0 {
+				// it wasn't cancelled between checking to.cancel
+				// and acquirint t.conn.mu
+				to.f()
+			}
 			t.conn.mu.Unlock()
 		}
 		t.mu.Unlock()
