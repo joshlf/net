c1c48c0787535a46f32dfbd061a2619991716d90
Joshua Liebow-Feeser <hello@joshlf.com>
2016-10-09 13:47:33 -0700

    tcp: timeouter now handles waiting for new timeouts when there are none

--- tcp/timeout.go
+++ tcp/timeout.go
@@ -16,6 +16,9 @@ type timeout struct {
 type timeouter struct {
 	conn     *Conn
 	timeouts heapTimeouts
+	// used when len(timeouts) == 0 and the daemon
+	// needs to wait until there are more timeouts
+	wg sync.WaitGroup
 
 	mu sync.Mutex
 }
@@ -24,31 +27,42 @@ type timeouter struct {
 func (t *timeouter) addTimeout(to *timeout) {
 	t.mu.Lock()
 	heap.Push(&t.timeouts, to)
+	if len(t.timeouts) == 1 {
+		// there were previously 0 which means that
+		// the daemon is blocking on t.wg
+		t.wg.Done()
+	}
 	t.mu.Unlock()
 }
 
 func (t *timeouter) daemon() {
 	for {
-		// TODO(joshlf): Check for the Conn having been closed, and return
+		t.mu.Lock()
+		if len(t.timeouts) == 0 {
+			// no timeouts; block until one is available
+			t.wg.Add(1)
+			t.mu.Unlock()
+			t.wg.Wait()
+			// there's a timeout now, so restart the loop
+			// and do the normal sleep thing
+			continue
+		}
+
+		// poor man's peek
+		to := heap.Pop(&t.timeouts).(*timeout)
+		heap.Push(&t.timeouts, to)
+		t.mu.Unlock()
+		time.Sleep(to.t.Sub(time.Now()))
 
 		t.mu.Lock()
-		to := heap.Pop(&t.timeouts).(*timeout)
+		to = heap.Pop(&t.timeouts).(*timeout)
 		if atomic.LoadUint32(&to.cancel) == 0 {
 			// it wasn't cancelled
 			t.conn.mu.Lock()
 			to.f()
 			t.conn.mu.Unlock()
 		}
-
-		if len(t.timeouts) > 0 {
-			// poor man's peek
-			to = heap.Pop(&t.timeouts).(*timeout)
-			heap.Push(&t.timeouts, to)
-			t.mu.Unlock()
-			time.Sleep(to.t.Sub(time.Now()))
-		} else {
-			// TODO(joshlf)
-		}
+		t.mu.Unlock()
 	}
 }
 
